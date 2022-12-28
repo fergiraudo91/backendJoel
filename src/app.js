65030d311d5d6d81eb9f0d8516e8f944ba6cea09
joelschachtel <joelschachtel@hotmail.com>
2022-12-27 23:45:10 -0300

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io';
+import __dirname from './utils.js';
 import productRouter from './routes/products.router.js';
+
+
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+const httpServer = app.listen(8080, () => {
+    console.log('Server listening on port 8080...');
+});
 
-app.use('/static', express.static('public'))
-app.use('/api/products', productRouter)
+const socketServer = new Server(httpServer);
 
-app.listen(8080, () => {
-    console.log('Servidor funcionando en puerto 8080...');
+app.use(express.json());
+app.use(express.static(__dirname + '/public/'));
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+app.use((req, res, next) => {
+    req.io = socketServer;
+    next();
+});
+app.use('/api/products', productRouter);
+
+socketServer.on('connection', socket =>{
+    console.log(socket.id);
+    socket.on('msg_front', message => console.log(message));
+    socket.emit('msg_back', 'Conectando al servicio, Bienvenido al backend.');
 })
+
+
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -5,7 +5,7 @@ const fileManager = new FileManager('products.json')
 const router = Router();
 // const products = [];
 
-router.get('/', async (req, res) => {
+router.get('/products', async (req, res) => {
     const products = await fileManager.get()
     let limit = req.query.limit
     if (!limit) res.send({products})
@@ -15,27 +15,56 @@ router.get('/', async (req, res) => {
         for(let index = 0; index < limit; i++) {
             prodLimit.push(products[index]);
         }
-    res.send({prodLimit})
+    res.send({prodLimit});
     }
+    req.io.emit('updatedProducts', products);
+})
+
+router.get('/products/:pid', async (req, res) => {
+    const id = req.params.pid;
+    const product = await FileManager.getById(id);
+    res.send({product});
 })
 
 router.post('/', async (req, res) => {
     const {title, description, price, thumbnails, code, stock, category, status} = req.body
-    const addProduct = await fileManager.add(title, description, price, thumbnails, code, stock, category, status)
-    res.send(addProduct)
+    const addProduct = await fileManager.add(title, description, price, thumbnails, code, stock, category, status);
+    req.io.emit('updatedProducts', await fileManager.get());
+    res.send(addProduct);
 })
 
 router.put('/:pid', async (req, res) => {
-    const id = parseInt(req.params.pid)
+    const id = parseInt(req.params.pid);
     const {title, description, price, thumbnails, code, stock, category, status} = req.body
-    const updateProduct = await fileManager.updateById(title, description, price, thumbnails, code, stock, category, status)
-    res.send(updateProduct)
+    const updateProduct = await fileManager.updateById(title, description, price, thumbnails, code, stock, category, status);
+    req.io.emit('updatedProducts', await fileManager.get());
+    res.send(updateProduct);
 })
 
 router.delete('/:pid', async (req, res) => {
     const id = parseInt(req.params.pid)
     const deleteProduct = await fileManager.deleteById(id)
+    req.io.emit('updatedProducts', await fileManager.get())
     res.send(deleteProduct)
+})
+
+router.get('/home', async (req, res) => {
+    const products = await fileManager.get();
+    res.render('home', 
+    {
+        title: 'Products list',
+        products: products
+    })
+})
+
+router.get('/realtimeproducts', async (req, res) =>{
+    const products = await fileManager.get()
+    res.render('realTimeProducts',
+    {
+        title: "Lista de Productos",
+        products: products
+    })
+
 })
 
 export default router;
